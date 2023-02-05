04b3ff43fa0606c9afe3604a2cf9a69e2d7c227b
Adam Eastwood <adam-3@live.co.uk>
2023-02-05 21:20:02 +0000

    Test commit

--- BikePSS/Program.cs
+++ BikePSS/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    Console.WriteLine("shit");
+    System.Threading.Thread.Sleep(1000);    
+}
